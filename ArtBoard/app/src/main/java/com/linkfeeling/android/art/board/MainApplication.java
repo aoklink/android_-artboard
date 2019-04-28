91609090c5d1bac200927d4e46580c704a6c9333
LegendaryPander <kill.god.sky@gmail.com>
2019-04-28 15:31:27 +0800

    【需求】bugly

---
+++ ArtBoard/app/src/main/java/com/linkfeeling/android/art/board/MainApplication.java
@@ -0,0 +1,20 @@
+package com.linkfeeling.android.art.board;
+
+import android.app.Application;
+
+import com.tencent.bugly.crashreport.CrashReport;
+
+/**
+ * Created on 2019/4/28  15:29
+ * chenpan pan.chen@linkfeeling.cn
+ */
+public final class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //        if (!BuildConfig.ENABLE_DEBUG) {
+        //bugly
+        CrashReport.initCrashReport(getApplicationContext(), "40cc86aadc", false);
+//        }
+    }
+}
